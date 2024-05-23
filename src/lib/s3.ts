a58bab9bd6dd18ab746ac3730c083b62468c8fa8
Nirmalya Nayak <n.nayak263@gmail.com>
2024-05-23 12:09:57 +0530

    change storage form r2 to s3

--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -4,11 +4,10 @@ import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { env } from "@/env";
 
 const s3Client = new S3Client({
-  region: "auto",
-  endpoint: `https://${env.CLOUDFLARE_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+  region: "ap-south-1",
   credentials: {
-    accessKeyId: env.CLOUDFLARE_ACCESS_KEY_ID,
-    secretAccessKey: env.CLOUDFLARE_SECRET_ACCESS_KEY,
+    accessKeyId: env.ACCESS_KEY_ID,
+    secretAccessKey: env.SECRET_ACCESS_KEY,
   },
 });
 
